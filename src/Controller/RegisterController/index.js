f204798a75120c0ac053aed6b5573a5ca3ac568f
Bhavuk-Devex <bhavukbansal@devexhub.in>
2024-11-14 19:02:29 +0530

    add otp validation

--- src/Controller/RegisterController/index.js
+++ src/Controller/RegisterController/index.js
@@ -1,4 +1,5 @@
 const loginModal = require("../../Modal/login/login");
+const otpModal = require("../../Modal/otp/otp");
 const bcrypt = require("bcrypt");
 
 const data = (req, res) => {
@@ -37,5 +38,78 @@ const login = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+const sendOTP = async (req, res) => {
+  console.log("Api hit");
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  const otpExpiry = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes
+  const { email } = req.body;
+  try {
+    await otpModal.findOneAndUpdate(
+      { email },
+      { otp, otpExpiry },
+      { upsert: true, new: true }
+    );
 
-module.exports = { data, login };
+    res.status(200).json({
+      success: true,
+      message: `otp send successfully to ${email}`,
+      otp,
+    });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+
+  const sendOTP = async (req, res) => {
+    console.log("Api hit");
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const otpExpiry = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes
+    const { email } = req.body;
+    try {
+      await otpModal.findOneAndUpdate(
+        { email },
+        { otp, otpExpiry },
+        { upsert: true, new: true }
+      );
+
+      res.status(200).json({
+        success: true,
+        message: `otp send successfully to ${email}`,
+        otp,
+      });
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+};
+
+const verifyOTP = async (req, res) => {
+  const { email, enteredOTP } = req.body;
+  const user = await otpModal.findOne({ email });
+
+  console.log(req.body);
+  console.log(user);
+  try {
+    if (user && user.otp === enteredOTP && user.otpExpiry > new Date()) {
+      user.otp = enteredOTP;
+      await user.save();
+      res.status(200).json({
+        success: true,
+        message: `OTP verified successfully`,
+        enteredOTP,
+      });
+    } else {
+      res.status(200).json({
+        success: false,
+        message: "Invalid or expired OTP",
+        enteredOTP,
+      });
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { data, login, sendOTP, verifyOTP };
--- src/Routes/RegisterRoutes.js
+++ src/Routes/RegisterRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { data, login } = require("../Controller/RegisterController");
+const {
+  data,
+  login,
+  sendOTP,
+  verifyOTP,
+} = require("../Controller/RegisterController");
 
 router.get("/data", data);
 router.post("/login", login);
+router.post("/otp", sendOTP);
+router.post("/verifyOTP", verifyOTP);
 
 module.exports = router;
